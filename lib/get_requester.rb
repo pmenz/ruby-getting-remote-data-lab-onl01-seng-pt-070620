9913d51a1029b845c294a6fe259ee5faf1e96241
Pablo Menz <p.menz.r@gmail.com>
2020-08-18 03:04:18 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
-# Write your code here
+require 'open-uri'  #loading module fo URI
+require 'net/http'  #loding module for net/http
+require 'json'      #laofind module for json
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
